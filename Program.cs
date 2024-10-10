c0bebf0d483c9b16e67748a10345a49b4aa69bea
Edson Yamamoto <edsonk.yamamoto@gmail.com>
2024-10-10 12:56:50 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpsRedirection(opt => opt.HttpsPort = 443);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
